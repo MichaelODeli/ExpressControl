c459c9809f561d252edc6fc692a26aae832075b4
Daniel Plotnikov <daniilrew@gmail.com>
2025-05-28 23:28:26 +0500

    simplify and sizes
    - упрощение кода для генерации светофора (убрано дублирование кода)
    - добавлены размеры светофоров

--- ExpressControl-Panel/pages/tests/train_lights.py
+++ ExpressControl-Panel/pages/tests/train_lights.py
@@ -4,62 +4,49 @@ from dash import Input, Output, callback, register_page, html
 register_page(__name__, path="/tests/train_lights", icon="fa-solid:home")
 
 
-def light_generator(light_type: str, light_parameters: tuple):
+def light_generator(light_type: str, light_parameters: tuple, light_size: str = "md"):
     """Генератор светофоров в HTML
 
     Args:
+        light_type (str): строка с типом светофора.
         light_parameters (tuple): список, описывающий конструкцию светофора.
-
-    Для вывода двухлинзового светофора следует сделать вложенный список со словарем формата `[{"red": "on", "green": "off"}]`
-    Для вывода пятилинзового светофора с разделением следует сделать список словарей вида `[{"red": "on", "green": "off", "yellow": "off"}, {"red": "on", "green": "off"}]`
     """
 
     light_group = []
 
-    if light_type == "diamond":
-        light = light_parameters[0]
-        light_color = light["color"]
-        light_state = light["state"]
-        light_blink = light["blink"]
-        blinker_class = " train_light-blinker " if light_blink and light_state else " "
-        return dmc.Flex(
-            [
+    for lights in light_parameters:
+        light_stack = []
+        for light in lights:
+            light_color = light["color"]
+            light_state = light["state"]
+            light_blink = light["blink"]
+            blinker_class = (
+                "train_light-blinker " if light_blink and light_state else " "
+            )
+
+            light_stack += [
                 html.Button(
-                    className="train_light-diamond-light"
+                    className=f"train_light-{light_type}-light "
+                    + f"train_light-{light_type}-light-{light_size} "
                     + blinker_class
                     + f"train_light-color-{light_color if light_state else 'off'}",
                 )
-            ],
-            className="train_light-diamond-box",
-            align="center",
-            justify="center",
-        )
-    else:
-        for lights in light_parameters:
-            light_stack = []
-            for light in lights:
-                light_color = light["color"]
-                light_state = light["state"]
-                light_blink = light["blink"]
-                blinker_class = (
-                    " train_light-blinker " if light_blink and light_state else " "
-                )
-
-                light_stack += [
-                    html.Button(
-                        className="train_light-classic-light"
-                        + blinker_class
-                        + f"train_light-color-{light_color if light_state else 'off'}",
-                    )
-                ]
-            light_group += [
-                dmc.Stack(light_stack, gap="xs", className="train_light-classic-box")
             ]
 
-        return dmc.Stack(
-            light_group,
-            gap="0",
-        )
+        light_group += [dmc.Flex(
+            children=light_stack,
+            className=f"train_light-{light_type}-box "
+            + f" train_light-{light_type}-box-{light_size}",
+            align="center",
+            justify="center",
+            gap='0' if light_type == 'diamond' else 'xs'
+        )]
+    
+
+    return dmc.Stack(
+        light_group,
+        gap="0",
+    )
 
 
 def layout():
@@ -106,38 +93,101 @@ def layout():
                             ],
                         ],
                     ),
-                    light_generator(
-                        "classic",
-                        [
-                            [
-                                {"color": "yellow", "state": False, "blink": True},
-                                {"color": "green", "state": True, "blink": False},
-                            ],
-                            [
-                                {"color": "red", "state": False, "blink": False},
-                                {"color": "yellow", "state": False, "blink": False},
-                            ],
-                        ],
-                    ),
                 ]
             ),
             dmc.Group(
                 [
                     light_generator(
                         "diamond",
-                        [{"color": "yellow", "state": False, "blink": False}],
+                        [[{"color": "yellow", "state": False, "blink": False}]],
                     ),
                     light_generator(
                         "diamond",
-                        [{"color": "yellow", "state": True, "blink": False}],
+                        [[{"color": "yellow", "state": True, "blink": False}]],
                     ),
                     light_generator(
                         "diamond",
-                        [{"color": "yellow", "state": True, "blink": True}],
+                        [[{"color": "yellow", "state": True, "blink": True}]],
                     ),
                     light_generator(
                         "diamond",
-                        [{"color": "green", "state": True, "blink": False}],
+                        [[{"color": "green", "state": True, "blink": False}]],
+                    ),
+                ],
+                gap="lg",
+            ),
+            dmc.Group(
+                [
+                    light_generator(
+                        "classic",
+                        [
+                            [
+                                {"color": "yellow", "state": False, "blink": False},
+                                {"color": "green", "state": False, "blink": False},
+                            ],
+                            [
+                                {"color": "red", "state": True, "blink": False},
+                                {"color": "yellow", "state": False, "blink": False},
+                            ],
+                            [
+                                {"color": "white", "state": True, "blink": False},
+                            ],
+                        ],
+                    ),
+                ],
+                align="flex-start",
+            ),
+            dmc.Group(
+                [
+                    light_generator(
+                        "classic",
+                        [
+                            [
+                                {"color": "red", "state": False, "blink": False},
+                                {"color": "purple", "state": False, "blink": False},
+                            ],
+                        ],
+                        light_size="sm",
+                    ),
+                    light_generator(
+                        "classic",
+                        [
+                            [
+                                {"color": "red", "state": False, "blink": False},
+                                {"color": "purple", "state": True, "blink": False},
+                            ],
+                        ],
+                        light_size="md",
+                    ),
+                    light_generator(
+                        "classic",
+                        [
+                            [
+                                {"color": "red", "state": True, "blink": False},
+                                {"color": "purple", "state": False, "blink": False},
+                            ],
+                        ],
+                        light_size="lg",
+                    ),
+                ],
+                align="flex-start",
+            ),
+            dmc.Group(
+                [
+                    light_generator(
+                        "diamond",
+                        [[{"color": "red", "state": True, "blink": False}]],
+                        light_size="sm",
+                    ),
+                    light_generator(
+                        "diamond",
+                        [[{"color": "green", "state": True, "blink": False}]],
+                        light_size="md",
+                    ),
+                    light_generator(
+                        "diamond",
+                        [[{"color": "white", "state": True, "blink": False}]],
+                        light_size="lg",
                     ),
                 ],
                 gap="lg",
